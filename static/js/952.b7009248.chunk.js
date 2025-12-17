89f21d9539dd2fe7312ff0024b10bbac5625d3c6
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2025-12-17 17:52:05 +0100

    Deploy Beta Version of Klokking 2026 App

---
+++ static/js/952.b7009248.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkklokking_2026=self.webpackChunkklokking_2026||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.b7009248.chunk.js.map
